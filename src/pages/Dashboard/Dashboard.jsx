551ce923c90212376612ab65400e5964e712b199
Michał Rejdych <rejdych.m.r@gmail.com>
2020-07-13 20:53:33 +0200

    Added basic dashboard layout

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Container, Grid, Paper, makeStyles } from '@material-ui/core';
+import { Container, Grid, Paper, makeStyles, Divider } from '@material-ui/core';
 
 const useClasses = makeStyles((theme) => ({
   root: {
     position: 'relative',
     top: theme.spacing(10),
+  },
+  paper: {
+    padding: 30,
+  },
+  item: {
+    height: 500,
+  },
+  paperInside: {
+    height: '90%',
   },
 }));
 
@@ -14,7 +23,18 @@ const Dashboard = () => {
   return (
     <Container disableGutters className={classes.root}>
       <Paper className={classes.paper}>
-        <Grid container></Grid>
+        <Grid container spacing={3}>
+          <Grid className={classes.item} item xs={3}>
+            <Paper className={classes.paperInside} elevation={3}></Paper>
+          </Grid>
+          <Divider />
+          <Grid className={classes.item} item xs={6}>
+            <Paper className={classes.paperInside} elevation={3}></Paper>
+          </Grid>
+          <Grid className={classes.item} item xs={3}>
+            <Paper className={classes.paperInside} elevation={3}></Paper>
+          </Grid>
+        </Grid>
       </Paper>
     </Container>
   );
