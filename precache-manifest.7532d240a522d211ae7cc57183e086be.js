cd0633cbc11b6641bb18fb94746aaec430c4ce33
Michał Rejdych <rejdych.m.r@gmail.com>
2020-08-13 22:03:37 +0200

    Updates

---
+++ precache-manifest.7532d240a522d211ae7cc57183e086be.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7def4c22968f951a1d78939a49cccd",
+    "url": "/Self-Board/index.html"
+  },
+  {
+    "revision": "11224f60a15fcbde3ef9",
+    "url": "/Self-Board/static/js/2.a286f279.chunk.js"
+  },
+  {
+    "revision": "cf5bd6fc1a1592a339ce0b88d38cf3c7",
+    "url": "/Self-Board/static/js/2.a286f279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c99145a8250b88058d8",
+    "url": "/Self-Board/static/js/main.24d84c1b.chunk.js"
+  },
+  {
+    "revision": "0d62341796fc3b615a15",
+    "url": "/Self-Board/static/js/runtime-main.98ccc893.js"
+  },
+  {
+    "revision": "30cf3550a5d802bf11cf02568932fedf",
+    "url": "/Self-Board/static/media/CalendarSvg.30cf3550.svg"
+  },
+  {
+    "revision": "b3e71a52519052af757ef8bbdc7e924e",
+    "url": "/Self-Board/static/media/DialogSvg.b3e71a52.svg"
+  },
+  {
+    "revision": "90b43a40b8833c6e2bb2e40c45c5073e",
+    "url": "/Self-Board/static/media/LandingPageSvg.90b43a40.svg"
+  },
+  {
+    "revision": "d52f5bd2a5c65cdf4bc8da141b007f8c",
+    "url": "/Self-Board/static/media/NewsFeedSvg.d52f5bd2.svg"
+  },
+  {
+    "revision": "ef2fd27317c49880dda04ca9a744f26e",
+    "url": "/Self-Board/static/media/TodoSvg.ef2fd273.svg"
+  }
+]);
